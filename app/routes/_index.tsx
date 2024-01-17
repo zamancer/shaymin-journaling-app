c5be1e83a886c0112b3a2b3d3c122c2cfcdc4ce2
Alan Zambrano <zamancer@Zamtable-Laptop.local>
2024-01-17 16:50:45 -0600

    feat: add Tailwind and migrate styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,50 +13,35 @@ export const meta: MetaFunction = () => {
 export const loader = () => {
   const contacts = getContactList();
   return json(contacts);
-}
+};
 
 export default function Index() {
   const data = useLoaderData<typeof loader>();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Say Thanks</h1>
+      <h1 className="text-3xl font-bold">Say Thanks</h1>
       <h2>Contacts</h2>
 
-      <ul 
-      style={
-        {
-          listStyle: "none",
-          padding: "0",
-          margin: "0",
-          maxWidth: "500px",
-        }
-      }
-      >
+      <ul className="list-none max-w-xl">
         {data.map((contact) => (
           <li key={contact.id}>
-            <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  padding: "0.5rem",
-                  border: "1px solid #ccc",
-                  borderRadius: "5px",
-                  margin: "0.5rem 0",
-                }}
-              >
-                <div>
-                  <h3>{contact.name}</h3>
-                  <p>Last Contacted: {prettyLastContact(contact.lastContacted)}</p>
-                </div>
-                <div>
-                  <a href={`/contacts/${contact.id}`}>
-                  Send Thanks
-                  </a>
-                </div>
+            <div className="flex items-center justify-between p-2 border-2 border-gray-300 rounded-md my-2">
+              <div>
+                <h3>{contact.name}</h3>
+                <p>
+                  Last Contacted: {prettyLastContact(contact.lastContacted)}
+                </p>
               </div>
-            
+              <div>
+                <a
+                  href={`/contacts/${contact.id}`}
+                  className="underline hover:no-underline"
+                >
+                  Send Thanks
+                </a>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
