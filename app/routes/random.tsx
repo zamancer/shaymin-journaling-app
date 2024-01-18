d73eff656ef4c083ea5d6fec4371bddf9b7539e4
Alan Zambrano <zamancer@Zamtable-Laptop.local>
2024-01-17 19:16:59 -0600

    feat: update random route with card component

--- app/routes/random.tsx
+++ app/routes/random.tsx
@@ -4,6 +4,7 @@ import { getContactList } from "~/lib/data/db.server";
 
 import { prettyPrintTimestamp } from "~/lib/utils";
 import { getRandomContact } from "~/lib/engines/random";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
 
 export const loader: LoaderFunction = async () => {
   const allContacts = getContactList();
@@ -15,10 +16,20 @@ export default function RandomContact() {
   const contact = useLoaderData<typeof loader>();
 
   return (
-    <div>
-      <h1>Contact to Reach Out</h1>
-      <p>Name: {contact.name}</p>
-      <p>Last Contacted: {prettyPrintTimestamp(contact.lastContacted)}</p>
-    </div>
+      <main className="flex justify-center py-4">
+        <Card className="lg:min-w-[400px]">
+          <CardHeader>
+            <CardTitle>{contact.name}</CardTitle>
+            <CardDescription>
+            Last Contacted: {prettyPrintTimestamp(contact.lastContacted)}
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <h3 className="text-lg font-semibold">Notes</h3>
+            <p>{contact.notes}</p>
+          </CardContent>
+        </Card>
+      </main>
   );
 }
